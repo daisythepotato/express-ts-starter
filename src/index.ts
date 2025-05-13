718d74ffb6f2bc047cea0e23050d8753c7f0bbab
Tulipurple <khlim0412@gmail.com>
2025-05-14 05:20:59 +0900

    feat : Add POST /users, DELETE /users/:id) and /hello route
    - 'GET /hello' 라우터 추가
    - 'POST /users' 유저 생성 기능 구현
    - 'DELETE /user/:id'로 유저 삭제 기능 구현

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 //서버 진입점 : 프로젝트의 시작, 필요한 패키지와 라우터 불러오기
 
 import express from 'express';
+import helloRouter from './routes/hello';
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
