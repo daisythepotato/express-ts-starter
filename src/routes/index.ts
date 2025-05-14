e8edff5291ae745696120089ee61a76dfee4c105
ouchdang <165886384+ldy03@users.noreply.github.com>
2025-05-14 23:07:47 +0900

    Revert "feat: PUT /users/:id, GET /status, GET /ping"

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,9 @@
-// 루트 라우터: '/' 요청을 처리하는 파일
+//루트 라우터 : '/' 요청을 처리하는 파일
 import express from 'express';
 const router = express.Router();
 
-// GET /: 서버에서 데이터 가져오기
 router.get('/', (req, res) => {
-    res.send('Welcome to the API!');
+  res.send('Welcome to the API!');
 });
 
 export default router;
